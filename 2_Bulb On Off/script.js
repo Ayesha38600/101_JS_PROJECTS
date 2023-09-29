a26555141cfeb6c8913ca8a08dbd178ae4fc0c75
AYESHA USMAN <113018202+Ayesha38600@users.noreply.github.com>
2023-09-29 12:32:43 +0500

    Add files via upload

---
+++ 2_Bulb On Off/script.js
@@ -0,0 +1,14 @@
+document.getElementById("img").src = "Of.png"
+document.getElementById("text").innerHTML = "OF"
+
+function btnon(){
+document.getElementById("img").src = "On.png"
+document.getElementById("text").innerHTML ="On"
+document.getElementById("on").style.backgroundColor = "rgb(243, 243, 157)"
+
+}
+function btnof(){
+document.getElementById("img").src = "Of.png"
+document.getElementById("text").innerHTML ="OFF!"
+document.getElementById("on").style.backgroundColor = "white"
+}
